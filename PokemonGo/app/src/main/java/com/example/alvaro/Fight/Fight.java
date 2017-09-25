36287925c6ba0e2562cdabbba01efac185421863
t00037790 <izzymovil@gmail.com>
2017-09-25 05:20:16 -0500

    full fight

--- PokemonGo/app/src/main/java/com/example/alvaro/Fight/Fight.java
+++ PokemonGo/app/src/main/java/com/example/alvaro/Fight/Fight.java
@@ -1,6 +1,7 @@
 package com.example.alvaro.Fight;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,10 +18,9 @@ import com.example.alvaro.Singleton.HttpRequest;
 import com.example.alvaro.pokemongo.R;
 import com.example.alvaro.winner.Winner;
 
-import java.math.RoundingMode;
+
 import java.util.ArrayList;
 import java.util.Random;
-import java.text.NumberFormat;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -36,8 +36,7 @@ public class Fight extends AppCompatActivity {
     TextView name1, name2;
     ListView fighting;
     ArrayList<String> hits;
-
-
+    int decrease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,19 +49,19 @@ public class Fight extends AppCompatActivity {
         name2 = (TextView) findViewById(R.id.poke2);
         fighting = (ListView) findViewById(R.id.results);
         hits = new ArrayList<String>();
-        ArrayAdapter<String> Adapter_fight= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , hits);
+        final ArrayAdapter<String> Adapter_fight= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , hits);
 
         Intent intent = getIntent();
 
         final String url = intent.getStringExtra("url");
         final String pic_f_winner = intent.getStringExtra("pic");
-        String  url2 = intent.getStringExtra("url2");
+        final String  url2 = intent.getStringExtra("url2");
         final String name_pokemon1 = intent.getStringExtra("pokemon1");
-        String name_pokemon2 = intent.getStringExtra("pokemon2");
+        final String name_pokemon2 = intent.getStringExtra("pokemon2");
 
         /// getting pokemon's power
-        int power1 = Integer.parseInt(intent.getStringExtra("powerpok1"));
-        int power2 = Integer.parseInt(intent.getStringExtra("powerpok2"));
+        final int power1 = Integer.parseInt(intent.getStringExtra("powerpok1"));
+        final int power2 = Integer.parseInt(intent.getStringExtra("powerpok2"));
 
         /// loading images
         loadImage(img1, url);
@@ -71,16 +70,73 @@ public class Fight extends AppCompatActivity {
         /// setting pokemon's names
         name1.setText(name_pokemon1);
         name2.setText(name_pokemon2);
+
+        //setting pokemon's life
         life = 100;
         life2 = 100;
+
         progressbar = (ProgressBar) findViewById(R.id.progressBar);
         progressbar2 = (ProgressBar) findViewById(R.id.progressBar2);
-
-
         Random who = new Random();
-        attacker = who.nextInt(2);
-        final int esto = fighting(life, life2, attacker, power1, power2);
-        System.out.print("Hello world!");
+        while(life>=0 && life2>=0 ) {
+
+            attacker = who.nextInt(2);
+            decrease = fighting(life, life2, attacker, power1, power2);
+            if (decrease > 0) {
+                if (attacker == 0) {
+                    hits.add(name_pokemon1 + " is attacking");
+                    progressbar2.setProgress(life2 - decrease);
+                    life2 = life2 - decrease;
+
+                } else {
+                    progressbar.setProgress(life - decrease);
+                    life = life - decrease;
+                    hits.add(name_pokemon2 + " is attacking");
+                }
+            }
+            fighting.setAdapter(Adapter_fight);
+            decrease = 0;
+        }
+
+
+
+
+
+
+        // validating winner pokemom becuase of the new activity to be created
+        if (life<=0 || life2<=0){
+
+            if (life>0){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent winner = new Intent(Fight.this, Winner.class);
+                        winner.putExtra("winner", name_pokemon1);
+                        winner.putExtra("url", pic_f_winner);
+                        startActivity(winner);
+                    }
+                }, 3000);
+
+
+            }
+
+            if(life2>0){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent winner = new Intent(Fight.this, Winner.class);
+                        winner.putExtra("winner", name_pokemon2);
+                        winner.putExtra("url", url2);
+                        startActivity(winner);
+                    }
+                }, 3000);
+
+            }
+
+
+        }
+
+
         /*
         final Timer timer = new Timer();
         timer.scheduleAtFixedRate( new TimerTask()
@@ -97,61 +153,39 @@ public class Fight extends AppCompatActivity {
             }
         }, 0, 5000);
 
-
-        for (int i = 0; i < 9; i++) {
-            new Handler().postDelayed(new Runnable() {
+         new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
+                    if (attacker==0){
+                        progressbar2.setProgress(life2 - decrease);
+                        life2 = life2 - decrease;
+
+                    }
+                    else {
+                        progressbar.setProgress(life - decrease);
+                        life = life - decrease;
+
+                    }
 
                 }
-            }, 75000);
+            }, 7000);
 
-        }
         */
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (attacker==0){
-                    progressbar2.setProgress(life2 - esto);
-                    life2 = life2 - esto;
-                }
-                else {
-                    progressbar.setProgress(life - esto);
-                    life = life - esto;
-                }
-                Intent winner = new Intent(Fight.this, Winner.class);
-                winner.putExtra("winner", name_pokemon1);
-                winner.putExtra("url", pic_f_winner);
-                startActivity(winner);
-
-
-            }
-        }, 7000);
-
 
 
     }
 
     public int fighting (int vida1, int vida2, int who, int power1, int power2){
         if (who==0){
-            ///NumberFormat numberFormat = NumberFormat.getInstance();
-            ///numberFormat.setMaximumFractionDigits(0);
-            ///numberFormat.setRoundingMode( RoundingMode.DOWN);
-            ///int remove = Integer.parseInt(numberFormat.format(power1/10));
             int remove = (power1/10);
             vida2 = vida2 - remove;
-            return vida2;
+            return remove;
 
         }
         else{
-            ///NumberFormat numberFormat = NumberFormat.getInstance();
-            ///numberFormat.setMaximumFractionDigits(0);
-            ///numberFormat.setRoundingMode( RoundingMode.DOWN);
-            ///int remove2 = Integer.parseInt(numberFormat.format(power2/10));
             int remove2 = (power2/10);
             vida1 = vida1 - remove2;
-            return vida1;
+            return remove2;
         }
 
     }
