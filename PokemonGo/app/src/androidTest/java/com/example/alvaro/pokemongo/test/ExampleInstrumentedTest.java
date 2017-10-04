2fd03cf980206154cc2a2a41f52c87e5dde63487
t00037790 <izzymovil@gmail.com>
2017-10-04 04:53:53 -0500

    at least something

--- PokemonGo/app/src/androidTest/java/com/example/alvaro/pokemongo/test/ExampleInstrumentedTest.java
+++ PokemonGo/app/src/androidTest/java/com/example/alvaro/pokemongo/test/ExampleInstrumentedTest.java
@@ -1,9 +1,10 @@
-package com.example.alvaro.pokemongo;
+package com.example.alvaro.pokemongo.test;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,10 +13,16 @@ import static org.junit.Assert.*;
 /**
  * Instrumentation test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+     * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @BeforeClass
+    public static void setUpClass() {
+        throw new RuntimeException("Dummy error");
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +30,12 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.alvaro.pokemongo", appContext.getPackageName());
     }
+
+    public void test_does_it_works(){
+
+        assertTrue(20>5);
+
+    }
+
+
 }
